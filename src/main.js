e26b7ba6f1735e3b04c7ee805b704035abfa6671
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-11-14 18:34:09 -0800

    Removes placeholder pages from assets files.

--- src/main.js
+++ src/main.js
@@ -5,17 +5,12 @@ import './css/styles.css';
 import Calc from './js/calc';
 
 $(document).ready(function() {
-  $("button#age-lifex-form").submit(function(event) {
-    event.preventDefault(event);
-    const calc = new Calc(age, lifeX);
+  $("#age-lifex-form").submit((e) => {
+    e.preventDefault();
     const age = parseInt($('#inputtedAge').val());
     const lifeX = parseInt($('#inputtedLifeX').val());
-    
-    console.log(calc.jupAge());
+    const calc = new Calc(age, lifeX);
+    const mercuryResponse = `"On Mercury, you would be ${calc.mercAge()} years old. You would have ${calc.mercYrsLeft()} years left to live on the first planet from the Sun."`;
+    console.log(mercuryResponse);
   });
-
-
-
-
-
 });
